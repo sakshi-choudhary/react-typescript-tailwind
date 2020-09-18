c2e4b0394d2b0a1fc233b2cc5796045341fa7f2b
jagnani73 <yashjagnani73@gmail.com>
2020-09-18 19:56:46 +0530

    feat: add tailwind with purge

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import logo from "./logo.svg";
+import "./App.css";
+import "./tailwind.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="text-3xl text-blue-400">
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
